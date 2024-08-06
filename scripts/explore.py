46d24b95bdd7a5c82e22856d19e2c41d23e8217f
teslajoy <nasim@plenary.org>
2024-08-06 08:43:39 -0700

    explore unique values

--- scripts/explore.py
+++ scripts/explore.py
@@ -11,23 +11,37 @@ def read_json(path):
         print("Error decoding JSON: {}".format(e))
 
 
-subject = read_json("../data/raw/subject.json")
+def fetch_unique_values(entity):
+    unique_values = {}
 
-unique_values = {}
+    for record in entity:
+        for key, value in record.items():
+            if key not in unique_values:
+                unique_values[key] = set()
+            unique_values[key].add(value)
 
-for record in subject:
-    for key, value in record.items():
-        if key not in unique_values:
-            unique_values[key] = set()
-        unique_values[key].add(value)
-
-unique_values = {key: list(values) for key, values in unique_values.items()}
-
-unique_values['species']
-unique_values['sex']
-unique_values['ethnicity']
-unique_values['race']
-unique_values['cause_of_death'] # loinc code https://loinc.org/79378-6/ 
-unique_values['vital_status']
+    unique_values = {key: list(values) for key, values in unique_values.items()}
+    return unique_values
 
 
+subject = read_json("./data/raw/subject.json")
+diagnosis = read_json("./data/raw/diagnosis.json")
+researchsubject = read_json("./data/raw/researchsubject.json")
+
+
+subject_unique_values = fetch_unique_values(subject)
+diagnosis_unique_values = fetch_unique_values(diagnosis)
+researchsubject_unique_values = fetch_unique_values(researchsubject)
+
+
+subject_unique_values['species']
+subject_unique_values['sex']
+subject_unique_values['ethnicity']
+subject_unique_values['race']
+subject_unique_values['cause_of_death'] # loinc code https://loinc.org/79378-6/
+subject_unique_values['vital_status']
+
+diagnosis_unique_values['primary_diagnosis'] # requires harmonization -> snomed code, MONDO, ICD10
+
+researchsubject_unique_values['primary_diagnosis_condition'] # requires harmonization -> snomed code
+researchsubject_unique_values['primary_diagnosis_site'] # requires harmonization -> snomed code
