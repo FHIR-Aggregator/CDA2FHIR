4046c43e4a964fa5e00439e7529ed857e6517120
teslajoy <nasim@plenary.org>
2024-12-30 16:38:36 -0800

    update subset to json array

--- scripts/subset_files.py
+++ scripts/subset_files.py
@@ -2,11 +2,13 @@ import json
 import os
 import sys
 
+# gunzip -c file.*.json.gz | jq -s '[.[][]]' > files.json # if files are separated
+
 _dir = sys.argv[1] if len(sys.argv) > 1 else 'data/raw/'
 
 file_path = os.path.join(_dir, 'file.json')
 associated_project_path = os.path.join(_dir, 'file_associated_project.json')
-out_dir = os.path.join(_dir, 'file_subset') # data/raw/file_subset needs to exist
+out_dir = os.path.join(_dir, 'files_subset') # data/raw/file_subset needs to exist
 error_log = os.path.join(_dir, 'invalid_entries.json')
 
 os.makedirs(out_dir, exist_ok=True)
@@ -27,12 +29,32 @@ def validate_json(data):
         valid_entries.append(item)
     return valid_entries, invalid_entries
 
-def project2file(project, file_entry):
-    """for a CDA project write json file"""
+# def project2file(project, file_entry):
+#     """for a CDA project write json file"""
+#     filename = os.path.join(out_dir, f'files_project_{project}.json')
+#     with open(filename, 'a') as project_file:
+#         json.dump(file_entry, project_file)
+#         project_file.write("\n")
+
+
+def project2file(project, file_entry, out_dir):
+    """for a CDA project, write a JSON file with entries formatted as a JSON array."""
     filename = os.path.join(out_dir, f'files_project_{project}.json')
-    with open(filename, 'a') as project_file:
-        json.dump(file_entry, project_file)
-        project_file.write("\n")
+    _array = []
+
+    if os.path.exists(filename):
+        with open(filename, 'r', encoding='utf-8') as project_file:
+            try:
+                _array = json.load(project_file)
+            except json.JSONDecodeError as e:
+                print(f"Error reading existing JSON data from {filename}: {e}")
+
+    _array.append(file_entry)
+
+    with open(filename, 'w', encoding='utf-8') as project_file:
+        json.dump(_array, project_file, indent=4)
+    print(f"Entry added and file updated: {filename}")
+
 
 try:
     with open(file_path, 'r') as f:
@@ -62,6 +84,6 @@ for file_entry in valid_data:
     if alias is not None:
         project = alias_to_project.get(alias)
         if project:
-            project2file(project, file_entry)
+            project2file(project, file_entry, out_dir)
 
 print(f'files have been divided/saved by CDA projects in folder: {out_dir}.')
